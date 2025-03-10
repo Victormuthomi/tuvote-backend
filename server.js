6edc102c011898bdb654ec5c25be8c23b80ebe4c
muthomi100 <techeasesolns@gmail.com>
2025-03-10 15:37:31 +0300

    Initial commit.

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import electionRoutes from "./routes/electionRoutes.js";
+import candidateRoutes from "./routes/candidateRoutes.js";
+import voteRoutes from "./routes/voteRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoutes);
+app.use("/api/elections", electionRoutes);
+app.use("/api/candidates", candidateRoutes);
+app.use("/api/vote", voteRoutes);
+
+// Database Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Tuvote API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
